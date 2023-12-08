53763e937d2449acfed2941b10f70916ea571231
ivan3455 <92949894+ivan3455@users.noreply.github.com>
2023-12-08 11:27:34 +0200

    lab8 done

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using AspNetMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult ProductList()
+        {
+            var products = new List<Product>
+            {
+                new Product { ID = 1, Name = "Маргарита", Price = 299.99, CreatedDate = DateTime.Now },
+                new Product { ID = 2, Name = "Піца шаурма", Price = 310, CreatedDate = DateTime.Now.AddMinutes(10) },
+                new Product { ID = 3, Name = "4 м'яса", Price = 329.99, CreatedDate = DateTime.Now.AddMinutes(15) },
+                new Product { ID = 4, Name = "Вершкова", Price = 290, CreatedDate = DateTime.Now.AddMinutes(20) },
+                new Product { ID = 5, Name = "4 сири", Price = 210, CreatedDate = DateTime.Now.AddMinutes(25) },
+                new Product { ID = 6, Name = "Баварська", Price = 360.99, CreatedDate = DateTime.Now.AddDays(30) }
+            };
+            return View(products);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,103 +1,36 @@
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-DateTime currentTime = DateTime.Now;
-var ErrorLogsFilePath = Path.Combine(Directory.GetCurrentDirectory(), "ErrorLog/ErrorLogs.txt");
-app.UseDeveloperExceptionPage();
-
-app.Map("/", (context) =>
+if (app.Environment.IsDevelopment())
 {
-    var htmlFilePath = Path.Combine(Directory.GetCurrentDirectory(), "html/form.html");
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
 
-    if (File.Exists(htmlFilePath))
-    {
-        var htmlContent = File.ReadAllText(htmlFilePath);
+app.UseHttpsRedirection();
+app.UseStaticFiles();
 
-        return context.Response.WriteAsync(htmlContent);
-    }
-    else
-    {
-        context.Response.StatusCode = 404;
-        return Task.CompletedTask;
-    }
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+
+    endpoints.MapControllerRoute(
+        name: "product",
+        pattern:"Product/ProductList",
+        defaults: new { controller = "Product", action = "ProductList" });
+
 });
-
-app.Map("/Submit", (context) =>
-{
-    var htmlFilePath = Path.Combine(Directory.GetCurrentDirectory(), "html/submit.html");
-
-    if (File.Exists(htmlFilePath))
-    {
-        var htmlContent = File.ReadAllText(htmlFilePath);
-
-        if (context.Request.Method == "POST")
-        {
-            var inputValue = context.Request.Form["inputValue"];
-            var inputDateAndTime = DateTime.Parse(context.Request.Form["inputDateAndTime"]);
-
-            var cookieOptions = new CookieOptions
-            {
-                Expires = inputDateAndTime,
-                HttpOnly = false
-            };
-
-            context.Response.Cookies.Append("formData", inputValue, cookieOptions);
-
-            htmlContent = htmlContent.Replace("{{Message}}", "Дані були успішно збережені в Cookie.");
-        }
-        else
-        {
-            htmlContent = htmlContent.Replace("{{Message}}", "Неправильний метод запиту.");
-        }
-
-        context.Response.ContentType = "text/html; charset=utf-8";
-        return context.Response.WriteAsync(htmlContent);
-    }
-    else
-    {
-        context.Response.StatusCode = 404;
-        return Task.CompletedTask;
-    }
-});
-
-app.Map("/CheckCookieValue", (context) =>
-{
-    var htmlFilePath = Path.Combine(Directory.GetCurrentDirectory(), "html/checkCookieValue.html");
-
-    if (File.Exists(htmlFilePath))
-    {
-        var htmlContent = File.ReadAllText(htmlFilePath);
-
-        if (context.Request.Cookies.TryGetValue("formData", out var formData))
-        {
-            htmlContent = htmlContent.Replace("{{CookieValue}}", $"В Cookie були знайдені наступні значення: {formData}");
-        }
-        else
-        {
-            htmlContent = htmlContent.Replace("{{CookieValue}}", "В Cookie не знайдено жодних значень.");
-        }
-
-        context.Response.ContentType = "text/html; charset=utf-8";
-        return context.Response.WriteAsync(htmlContent);
-    }
-    else
-    {
-        context.Response.StatusCode = 404;
-        return Task.CompletedTask;
-    }
-});
-
-app.Map("/GenerateError", (context) =>
-{
-    throw new Exception("Це тестова помилка!");
-});
-
-app.UseExceptionHandler(app => app.Run(async context =>
-{
-    File.WriteAllText(ErrorLogsFilePath, "Помилка під час виконання запиту " + currentTime);
-    context.Response.StatusCode = 500;
-    await context.Response.WriteAsync("Error 500.");
-}));
 
 app.Run();
+
