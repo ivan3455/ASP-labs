c1bf1dd5664fc834d644478b417c4599d0d52925
ivan3455 <92949894+ivan3455@users.noreply.github.com>
2023-12-19 05:18:13 +0200

    lab done

--- Program.cs
+++ Program.cs
@@ -1,10 +1,18 @@
+using AspNetMVC.Filters;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews(options =>
+{
+    // ��������� ������� LogActionFilter
+    options.Filters.Add(typeof(LogActionFilter));
+
+    // ��������� ������� UniqueUsersFilter
+    options.Filters.Add(typeof(UniqueUsersFilter));
+});
 
 var app = builder.Build();
 
